6febcab0df33b22458df3a06aa5fe031925dcfde
Stephen Doty <stephendoty826@yahoo.com>
2021-12-28 11:30:07 -0600

    got emailjs working

---
+++ assets/js/emailjs.js
@@ -0,0 +1,18 @@
+
+import { init } from '../../node_modules/emailjs-com';
+init("user_0FstqH27JgLyTmrBT0ZDO");
+
+window.onload = function() {
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // generate a five digit number for the contact_number variable
+    this.contact_number.value = Math.random() * 100000 | 0;
+    // these IDs from the previous steps
+    emailjs.sendForm('contact_service', 'contact_form', this)
+      .then(function() {
+        console.log('SUCCESS!');
+      }, function(error) {
+        console.log('FAILED...', error);
+    });
+  });
+}
